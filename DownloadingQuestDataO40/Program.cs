70127c610559e379ce9bc51d258038c7e89f548d
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-02 23:55:39 -0600

    program finished

--- DownloadingQuestDataO40/Program.cs
+++ DownloadingQuestDataO40/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public static async Task Main()
+	{
+        string quest = await DownloadQuest(); 
+
+        //I learned trying to call Task DonwloadQuest 
+        // right here is like declaring a string that
+        // already has the word string on 
+        // it earlier in the code
+        Console.WriteLine($"New Quest: {quest}");
+    }
+
+    static async Task<string> DownloadQuest()
+    {
+        Console.WriteLine("Downloading quest data");
+        await Task.Delay(4600);
+        Console.WriteLine("Quest downloaded in 4.6 seconds");
+        return "Defeat the Ender Dragon";
+ 
+ 
+    }
+
+}
